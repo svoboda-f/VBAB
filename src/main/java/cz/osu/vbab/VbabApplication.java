9152fd7c774d3b95a330e5d664657a86c3a33c0d
svoboda-f <r19232@student.osu.cz>
2023-01-17 12:43:19 +0100

    Init

---
+++ src/main/java/cz/osu/vbab/VbabApplication.java
@@ -0,0 +1,13 @@
+package cz.osu.vbab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VbabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VbabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/osu/vbab/VbabApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.osu.vbab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VbabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
